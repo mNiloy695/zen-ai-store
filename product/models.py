261907a2e07978bd987b0ccd0ac3b34d7598d771
salah uddin <mniloy695@gmail.com>
2026-04-03 11:33:18 +0600

    migarte the product model

--- product/models.py
+++ product/models.py
@@ -3,9 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    name=models.CharField(max_length=250)
+    name=models.CharField(max_length=2000)
     description=models.TextField(null=True, blank=True)
     category=models.CharField(max_length=250, null=True, blank=True)
     
     def __str__(self):
         return f"id {self.id} - name: {self.name}"
+    
+    
+    
