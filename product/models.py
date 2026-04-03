bf30cd2d66bcec462ac7ea9639e499ff5e767df3
salah uddin <mniloy695@gmail.com>
2026-04-03 11:37:04 +0600

    in the product model a user field is added

--- product/models.py
+++ product/models.py
@@ -1,8 +1,10 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
+User=get_user_model()
 # Create your models here.
 
 class Product(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE, related_name='products')
     name=models.CharField(max_length=2000)
     description=models.TextField(null=True, blank=True)
     category=models.CharField(max_length=250, null=True, blank=True)
--- product/views.py
+++ product/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
-
+from .serializers import ProductSerializer
+from .models import Product
 # Create your views here.
