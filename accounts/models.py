ca57ad787cf0da3241fddd9e66e3bd8e754968a0
salah uddin <mniloy695@gmail.com>
2026-04-03 09:10:20 +0600

    for customer user management the custom model are builed

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser,BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self,email,password=None,**extra_fields):
+        if not email:
+            raise ValueError("Email must be provided")
+        email=self.normalize_email(email)
+        user=self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self,email,password=None,**extra_fields):
+        extra_fields.setdefault('is_staff',True)
+        extra_fields.setdefault('is_superuser',True)
+        
+        #validation for superuser
+        
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError("Superuser must have is_staff=True")
+        
+        
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError("Superuser must have is_superuser=True")
+        
+        return self.create_user(email,password,**extra_fields)
+    
+
+class CustomUser(AbstractUser):
+    username=None
+    email=models.EmailField(unique=True)
+    first_name=models.CharField(max_length=50,blank=True,null=True)
+    last_name=models.CharField(max_length=50,blank=True,null=True)    
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=[]
+    objects=CustomUserManager()
+    
+    def __str__(self):
+        return f'email : {self.email} - name  : {self.first_name} {self.last_name} and id is {self.id}'
+    
+    
+    
