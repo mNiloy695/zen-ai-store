2f2ff7d3041fb0b79c23881e1e2b642c32416024
salah uddin <mniloy695@gmail.com>
2026-04-03 09:25:09 +0600

    for user registration serializer created

---
+++ accounts/serializers.py
@@ -0,0 +1,36 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User=get_user_model()
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    confirm_password=serializers.CharField(write_only=True)
+    class Meta:
+        model=User
+        fields=['email','first_name','last_name','password','confirm_password']
+        
+    
+    def validate(self,attrs):
+        password=attrs.get('password')
+        confirm_password=attrs.pop('confirm_password')
+        email=attrs.get('email')
+        
+        if not password:
+            raise serializers.ValidationError("Password is required")
+        
+        if not confirm_password:
+            raise serializers.ValidationError("Confirm password is required")
+        
+        if password !=confirm_password:
+            raise serializers.ValidationError("Password and confirm password do not match")
+        
+        if User.objects.filter(email=email).exists():
+            raise serializers.ValidationError("A user with this email already exists")
+        
+        return attrs
+    
+    def create(self, validated_data):
+        validated_data.pop('confirm_password')
+        user = User.objects.create_user(**validated_data)
+        return user
