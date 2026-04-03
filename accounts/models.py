4dff8c7d9f649bbee0617d04fee0fe63e1a354fb
salah uddin <mniloy695@gmail.com>
2026-04-03 09:51:19 +0600

    UserProfile Model created

--- accounts/models.py
+++ accounts/models.py
@@ -42,3 +42,13 @@ class CustomUser(AbstractUser):
         return f'email : {self.email} - name  : {self.first_name} {self.last_name} and id is {self.id}'
     
     
+
+class UserProfile(models.Model):
+    user=models.OneToOneField(CustomUser,on_delete=models.CASCADE,related_name='profile')
+    avatar=models.ImageField(upload_to='avatars/',blank=True,null=True)
+    name=models.CharField(max_length=110,blank=True,null=True)
+    address=models.CharField(max_length=255,blank=True,null=True)
+    phone_number=models.CharField(max_length=20,blank=True,null=True)
+    
+    def __str__(self):
+        return f'Profile of {self.user.email}'
